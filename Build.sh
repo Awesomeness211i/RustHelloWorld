970e13b5a0187ac9818ba078d3017012c8f92a21
Awesomeness211i <chrisiman53@gmail.com>
2022-08-17 22:21:35 -0700

    + Changed name and function of build.sh + Added DebugRun.sh + Added ReleaseRun.sh

---
+++ Build.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+cargo build --profile=dev
+cargo build --profile=release
--- build.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-cargo build --profile=dev
-cargo build --quiet --profile=release
-cargo run --quiet
